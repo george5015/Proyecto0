46620c1e00444ff61a08014e2c7923634af1fdf3
jtwnel <30628660+jtwnel@users.noreply.github.com>
2018-02-01 22:41:57 -0500

    se agrega modelo de user y manejo de sesion

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,12 @@
 var express = require('express')
   , router = express.Router()
+var util = require('../lib/util.js');
 
-// Domestic animals page
+
+router.get('/login', sessionChecker, (req, res) => {
+  res.sendFile(__dirname + '/public/signup.html');
+})
+
 router.post('/login', (req, res) => {
 
 })
@@ -12,3 +17,44 @@ router.post('/register', (req, res) => {
 })
 
 module.exports = router
+
+app.route('/signup')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/signup.html');
+    })
+    .post((req, res) => {
+        User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        })
+        .then(user => {
+            req.session.user = user.dataValues;
+            res.redirect('/dashboard');
+        })
+        .catch(error => {
+            res.redirect('/signup');
+        });
+    });
+
+
+// route for user Login
+app.route('/login')
+    .get(sessionChecker, (req, res) => {
+        res.sendFile(__dirname + '/public/login.html');
+    })
+    .post((req, res) => {
+        var username = req.body.username,
+            password = req.body.password;
+
+        User.findOne({ where: { username: username } }).then(function (user) {
+            if (!user) {
+                res.redirect('/login');
+            } else if (!user.validPassword(password)) {
+                res.redirect('/login');
+            } else {
+                req.session.user = user.dataValues;
+                res.redirect('/dashboard');
+            }
+        });
+    });
--- server.js
+++ server.js
@@ -1,11 +1,36 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var app = express();
 var path = require('path');
+var util = require('./lib/util.js');
 var router = express.Router();
+
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
+app.use(cookieParser());
+
+// initialize express-session to allow us track the logged-in user across sessions.
+app.use(session({
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}));
+
+app.use((req, res, next) => {
+    if (req.cookies.user_sid && !req.session.user) {
+        res.clearCookie('user_sid');
+    }
+    next();
+});
+
+app.get('/', util.sessionChecker,(req, res) => {
   res.sendFile(__dirname + '/views' + '/index.html')
 });
 
